49d96c60c599a1fe040f6f0199dad2d907f00a41
davidgbogdan <davidgeorgebogdan@gmail.com>
2025-09-29 12:38:55 +0300

    Added my-work module

---
+++ my-work/src/main/java/org/itss/backtoschool/mywork/MyWorkApplication.java
@@ -0,0 +1,13 @@
+package org.itss.backtoschool.mywork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWorkApplication.class, args);
+    }
+
+}

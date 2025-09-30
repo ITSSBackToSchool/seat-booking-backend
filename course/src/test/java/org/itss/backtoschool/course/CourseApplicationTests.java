90998b8bd1cc442ef3e1ea2f5e14dfea8bc0240f
davidgbogdan <davidgeorgebogdan@gmail.com>
2025-09-30 14:42:17 +0300

    Added new dependencies for starting the project + changing the port for DB to avoid compatibility issues + name of db + refactored properties file to yml + scripts for insert in DB

--- course/src/test/java/org/itss/backtoschool/course/CourseApplicationTests.java
+++ course/src/test/java/org/itss/backtoschool/course/CourseApplicationTests.java
@@ -1,13 +1,13 @@
-package org.itss.backtoschool.course;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CourseApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.itss.backtoschool.course;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CourseApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}

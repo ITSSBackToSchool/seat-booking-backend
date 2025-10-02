62d9f3f8a03ed060537057422d1361b616607474
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-10-02 18:18:56 +0300

    Add custom exceptions

---
+++ deskops/src/main/java/org/itss/backtoschool/deskops/exception/seat/SeatNotFoundException.java
@@ -0,0 +1,5 @@
+package org.itss.backtoschool.deskops.exception.seat;
+
+public class SeatNotFoundException extends RuntimeException{
+
+}
---
+++ deskops/src/main/java/org/itss/backtoschool/deskops/exception/user/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package org.itss.backtoschool.deskops.exception.user;
+
+public class UserNotFoundException extends RuntimeException{
+
+}

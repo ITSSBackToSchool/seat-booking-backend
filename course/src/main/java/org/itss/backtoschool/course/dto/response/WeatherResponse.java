e8dc1ecc19480e9e4b50892c3ac52a1ffa4f74ec
Tudor Arabagiu <tudor.arabagiu@gmail.com>
2025-10-03 11:04:11 +0300

    Added weather api, need to set for daily weather

---
+++ course/src/main/java/org/itss/backtoschool/course/dto/response/WeatherResponse.java
@@ -0,0 +1,29 @@
+package org.itss.backtoschool.course.dto.response;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+@Getter
+@Setter
+public class WeatherResponse {
+    private double latitude;
+    private double longitude;
+    private double generationtime_ms;
+    private int utc_offset_seconds;
+    private String timezone;
+    private String timezone_abbreviation;
+    private double elevation;
+    private Map<String, String> current_units;
+    private CurrentWeather current;
+
+    @Getter
+    @Setter
+    public static class CurrentWeather {
+        private String time;
+        private int interval;
+        private double temperature_2m;
+        private int relative_humidity_2m;
+        private double precipitation;
+        private double surface_pressure;
+    }
+}
